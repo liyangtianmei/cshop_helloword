bfd98243c5ab1e550614704d8e8fc0469d06fc06
liyangtianmei <liyangtianmei@163.com>
2019-05-16 09:12:02 +0800

    添加项目文件。

---
+++ cshop_helloword/Helloword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cshop_helloword
+{
+    class Helloword
+    {
+        //主函数入口声明
+        static void Main(string[] args)
+        {
+            //显示消息
+            Console.WriteLine("Hello world!!");
+            Console.ReadKey();
+        }
+    }
+}
